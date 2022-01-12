21f073315326133fbc4ae87cc094210b0322b7c7
Ritik2712 <ritik2712mahajan@gmail.com>
2022-01-12 12:19:19 +0530

    initial coomit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import HomePage from './Components/HomePage';
+import Navbar from './Components/Navbar';
+import PostandLoc from './Components/PostandLoc';
+import Select from './Components/Select';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <HomePage />
+      <Select />
+      <PostandLoc />
     </div>
   );
 }
